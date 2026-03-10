4cd710966088350175c65f3061c8b4452e6e8eba
Vijay Guttula <vijayguttula001@gmail.com>
2026-03-10 20:42:20 +0530

    Supports env file

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes";
 import stationRoutes from "./routes/station.routes";
 
+dotenv.config();
+
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
